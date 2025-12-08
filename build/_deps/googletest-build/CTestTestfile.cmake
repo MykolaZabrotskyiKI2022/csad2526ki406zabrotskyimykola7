af10d7a5f066cdb979a813ae8f48c3217a416c46
MykolaZabrotskyi <mykola.zabrotskyi.ki.2022@lpnu.ua>
2025-12-08 14:44:23 +0200

    Add math_operations.cpp and initial build artifacts
    Added math_operations.cpp source file and generated build artifacts including CMake and Ninja configuration files, object files, and test binaries. This sets up the initial build environment and integrates GoogleTest for unit testing.

---
+++ build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mykolazabrotskyi/GitHub/csad2526ki406zabrotskyimykola7/build/_deps/googletest-src
+# Build directory: /Users/mykolazabrotskyi/GitHub/csad2526ki406zabrotskyimykola7/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mykolazabrotskyi/GitHub/csad2526ki406zabrotskyimykola7/build/_deps/googletest-src/googletest
+# Build directory: /Users/mykolazabrotskyi/GitHub/csad2526ki406zabrotskyimykola7/build/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
